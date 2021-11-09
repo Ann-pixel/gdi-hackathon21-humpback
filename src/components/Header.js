ef414f24fe320c9debd2c9635d19f470409d0222
ttucker <tracytuckeremail@gmail.com>
2021-11-09 17:53:45 -0600

    Adding emptty header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
