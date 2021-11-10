0cfaf769516303a7e1bf5e2e9a9449f92cf4e66c
ttucker <tracytuckeremail@gmail.com>
2021-11-10 08:52:43 -0600

    Adding styling to hr tag in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,24 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Header = () => {
+
+    const headerStyle={
+        margin: '0 auto',
+        paddingTop: '20px',
+        textAlign: 'center',
+    };
+
+    const hardLine={
+        borderTop: '3px double #8c8b8b',
+        width: '75%',
+    }
+
     return (
-        <Container>
+        <Container style={headerStyle}>
             <Row>
-                <Col><h1>Welcome to Simon Game</h1></Col>
+                <Col><h1>Welcome to the Simon Game</h1></Col>
+            </Row><Row>
+                <Col><hr style={hardLine}/></Col>
             </Row>
         </Container>
     )
