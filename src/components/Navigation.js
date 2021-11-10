cfeaa6c6bcfcc956ce02687899a088beb42e9807
ttucker <tracytuckeremail@gmail.com>
2021-11-10 07:22:03 -0600

    Adding navigation color and positioning

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,24 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const Navigation = () => {
+    const navStyle={
+        backgroundColor: '#005b7b',
+        color: 'white',
+        paddingTop: '20px',
+        paddingBottom: '20px',
+    };
     return (
-        <Navbar bg='light' variant="light" expand="md">
+        <Navbar bg="light" variant="light" expand="md" style={navStyle}>
             <Container>
                 <Navbar.Brand>
-                    working
+                    whale goes here
                 </Navbar.Brand>
+                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto Navbar">
+                        future navigation goes here 
+                    </Nav>
+                </Navbar.Collapse> */}
             </Container>
         </Navbar>
     )
